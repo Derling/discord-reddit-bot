556cbe4c08ac36f4187d6c5fefb0304885802917
Derling <derling0704@gmail.com>
2018-11-10 17:31:10 -0500

    Add main loop functionality to bot

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
 import discord
+import time
 
 from reddit.client import Client as Reddit
 from utils.lib import (
@@ -11,28 +12,32 @@ from utils.lib import (
 from utils.servers import SERVERS
 from utils.token import TOKEN
 
+POKE_INTERVAL = 1800 # look for new posts every 
+
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-	for server in SERVERS:
-		subreddit = server['subreddit']
-		posts = server['posts']
-		server_name = server['name']
-		server_channel = server['channel']
-		category = server.get('category', 'hot')
+	while True:
+		for server in SERVERS:
+			subreddit = server['subreddit']
+			posts = server['posts']
+			server_name = server['name']
+			server_channel = server['channel']
+			category = server.get('category', 'hot')
 
-		channel = get_channel(client, server_name, server_channel)
-		channel_messages = await get_channel_messages(client, channel, posts)
-		reddit_client = Reddit()
-		last_reddit_posts = reddit_client.get_posts(subreddit, category, posts)
+			channel = get_channel(client, server_name, server_channel)
+			channel_messages = await get_channel_messages(client, channel, posts)
+			reddit_client = Reddit()
+			last_reddit_posts = reddit_client.get_posts(subreddit, category, posts)
 
-		new_posts = get_new_posts(channel_messages, last_reddit_posts)
-		if new_posts:
-			for post in new_posts:
-				post_string = get_posts_string(post)
-				await client.send_message(channel, post_string)
+			new_posts = get_new_posts(channel_messages, last_reddit_posts)
+			if new_posts:
+				for post in new_posts:
+					post_string = get_posts_string(post)
+					await client.send_message(channel, post_string)
 
+		time.sleep(POKE_INTERVAL)
 
 client.run(TOKEN)
