0449f0c6cf48eabb1c75146c985b42a3291d8fed
Derling <derling0704@gmail.com>
2018-11-22 18:33:53 -0500

    Add utils lib test

--- test/utils/test_lib.py
+++ test/utils/test_lib.py
@@ -3,7 +3,6 @@ from mock import patch, Mock
 
 from utils.lib import (
 	get_channel,
-	get_channel_messages,
 	parse_messages,
 	get_new_posts,
 	get_posts_string
@@ -74,3 +73,36 @@ def test_get_channel_raises_error_when_client_belongs_to_server_but_invalid_chan
 		get_channel(client, server, channel)
 	assert str(error.value) == "Client belongs to server1 server but could not " \
 						 	   "find the fake_channel channel in the server"
+
+
+def test_parse_messages_returns_correct_strings():
+	assert parse_messages([
+		'title1 http://url_for_post',
+		'title2 http://url_for_second_post']
+	) == ['title1', 'title2']
+
+
+def test_get_new_posts_returns_new_posts():
+	old_posts = []
+	for title in ['old1', 'old2']:
+		mock_post = Mock()
+		mock_post.title = title
+		old_posts.append(mock_post)
+	
+	messages = [f'{p.title} http://url' for p in old_posts]
+	
+	new_posts = []
+	for title in ['new1', 'new2']:
+		mock_post = Mock()
+		mock_post.title = title
+		new_posts.append(mock_post)
+
+	assert get_new_posts(messages, old_posts + new_posts) == new_posts
+
+
+def test_get_posts_string_returns_correct_post_string():
+	mock_post = Mock()
+	mock_post.title = 'Mock Title For An Awesome Post'
+	mock_post.url = 'http://url.for/awesome_post'
+
+	assert get_posts_string(mock_post) == 'Mock Title For An Awesome Post http://url.for/awesome_post'
