0ba4e80126b5436eeb5c73c5eec019faaca62af6
Derling <derling0704@gmail.com>
2018-11-22 19:37:40 -0500

    Add comments about server definitions

--- utils/servers.py
+++ utils/servers.py
@@ -10,6 +10,22 @@ category(str): the reddit category to look at
 			   when pulling posts(defaults to "hot")
 name(str): the name of the server
 channel(str): the name of the channel
+
+Example: 
+
+In this example the bot will pull the last 100 posts from the hot "popular" category from the funny subreddit.
+It will then write messages about the posts in the "server-name" server in the "channel-name" channel.
+Where "server-name" and "channel-name" would be the actual names for the server and channel the bot belongs to.
+
+SERVERS = [
+	{
+		'subreddit': 'Funny',
+		'posts': 100,
+		'category': 'popular',
+		'name': 'server-name',
+		'channel': 'channel-name'
+	}
+]
 """
 
 SERVERS = [
